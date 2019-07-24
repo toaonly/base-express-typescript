84f0c52c2da8a39069cb18c1f3a59efc713e571e
toaonly42 <toaonly42@gmail.com>
2019-07-24 20:16:51 +0900

    modified webpack.dev.conf.js

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -3,13 +3,18 @@ const dotenv = require('dotenv')
 const WebpackDotenv = require('dotenv-webpack')
 const base = require('./webpack.base.conf')
 
-const envPath = path.join(__dirname, '../.env.development')
+const envPath = path.join(__dirname, '../.env')
+const envDevPath = path.join(__dirname, '../.env.development')
 
 dotenv.config({
   path: envPath
 })
 
 base.mode = process.env.NODE_ENV
-base.plugins.push(new WebpackDotenv({ path: envPath }))
+base.plugins.push(new WebpackDotenv({
+  path: envDevPath,
+  expand: true,
+  default: envPath
+}))
 
 module.exports = base
