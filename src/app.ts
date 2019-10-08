81ad3dcab01fd9b8db7797a1973702989da69757
toaonly42 <toaonly42@gmail.com>
2019-10-08 13:00:27 +0900

    cors 추가

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from 'express'
 import api from './api'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
 
 app.disable('x-powered-by')
+app.use(cors({
+  optionsSuccessStatus: 200
+}))
 app.use(bodyParser.json())
 
 app.use('/', api)
